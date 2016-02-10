407c3de803c1060f86edee5c98c74f4c341decb8
Vladimir Osintsev <osintsev@gmail.com>
2016-02-10 23:39:44 +0300

    BeerComponent added

--- app/components/beer.ts
+++ app/components/beer.ts
@@ -1,7 +1,22 @@
 import { Component } from 'angular2/core';
 
+import { BeerService } from '../services/beer';
+
 @Component({
     selector: 'beer',
-    template: '<h2>Beers</h2>'
+    template: `
+        <h2>Beers</h2>
+        <div *ngFor="#beer of beers">
+            {{beer.id}} - {{beer.name}}
+        </div>
+    `,
+    providers: [BeerService]
 })
-export class BeerComponent { }
+export class BeerComponent {
+
+    beers: any;
+
+    constructor(private _beerService: BeerService) {
+        this.beers = this._beerService.getBeerList();
+    }
+}
